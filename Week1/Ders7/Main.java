43f59b5561b24c892e92fca7326376777040b6d7
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-09-19 14:32:36 +0300

    Ders 7

---
+++ Week1/Ders7/Main.java
@@ -0,0 +1,15 @@
+package Ders7;
+
+public class Main {
+    public static void main(String[] args) {
+        double sayid = 12.5;
+        int sayi = 12;
+
+        String sehir = "Ankara";
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+
+    }
+}
