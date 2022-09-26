8d011bfce5e6b7b6204738bf8490be5e89961818
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-09-26 22:53:19 +0300

    Ders 28

--- Week2/Ders28/Ders28.java
+++ Week2/Ders28/Ders28.java
@@ -12,18 +12,6 @@ public class Ders28 {
         customerManager.Remove();
         customerManager.Update();
 
-        //value type
-        int sayi1 = 10;
-        int sayi2 = 20;
-        sayi2 = sayi1;
-        sayi1 = 30;
-        System.out.println(sayi2);
-
-        int[] sayilar1 = new int[] {1,2,3};
-        int[] sayilar2 = new int[] {4,5,6};
-        sayilar2 = sayilar1;
-        sayilar1[0]=10;
-        System.out.println(sayilar2[0]);
 
 
 
