2c428e0c8bd2b6a4ec83d3fa7b72599ca99b78ca
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-09-26 22:49:58 +0300

    Ders 29

---
+++ Week2/Ders29/Ders29.java
@@ -0,0 +1,12 @@
+package Ders29;
+
+public class Ders29 {
+
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.topla(3,4);
+        System.out.println(sonuc);
+
+    }
+}
