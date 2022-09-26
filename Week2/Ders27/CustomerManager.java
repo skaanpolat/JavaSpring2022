ee048a8e2f9a28ec581b9cd421fa2c445760302d
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-09-26 21:36:38 +0300

    Ders 27

---
+++ Week2/Ders27/CustomerManager.java
@@ -0,0 +1,16 @@
+package Ders27;
+
+public class CustomerManager {
+
+    public void Add() {
+        System.out.println("Müşteri eklendi!");
+    }
+
+    public void Remove() {
+        System.out.println("Müşteri eklendi!");
+    }
+
+    public void Update() {
+        System.out.println("Müşteri güncellendi!");
+    }
+}
---
+++ Week2/Ders27/Ders27.java
@@ -0,0 +1,13 @@
+package Ders27;
+
+public class Ders27 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+
+
