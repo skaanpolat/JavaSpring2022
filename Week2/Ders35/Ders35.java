bde8515f0c883e973546a54df60af77b9f51eeea
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-09-27 12:48:48 +0300

    Ders 35

---
+++ Week2/Ders35/Ders35.java
@@ -0,0 +1,14 @@
+package Ders35;
+
+public class Ders35 {
+    public static void main(String[] args) {
+
+
+        Dortislem dortislem = new Dortislem();
+
+        System.out.println(dortislem.topla(2,3));
+
+        dortislem.topla(2,3,5);
+
+    }
+}
