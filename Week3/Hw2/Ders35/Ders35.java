b36bb50647effda6223386442aad46dd096980cb
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-10-17 15:46:35 +0300

    W3-HW2-D35

---
+++ Week3/Hw2/Ders35/Ders35.java
@@ -0,0 +1,13 @@
+package Hw2.Ders35;
+
+public class Ders35 {
+
+    public static void main(String[] args) {
+
+        //method overloading
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(2, 3));
+        System.out.println(dortIslem.topla(2, 3, 4));
+
+    }
+}
